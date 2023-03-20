e26e880f0b3f36c4db99b4de43ac7bf33ad277e4
Thomas Petit <6poules@gmail.com>
2023-03-20 15:31:55 +0100

    [+] title header

--- hallownest_bingo/src/routes/+layout.svelte
+++ hallownest_bingo/src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
+  import { assets } from "$app/paths";
   import "../style.css";
 </script>
+
+<header>
+  <img src="%sveltekit.assets%/Husk_header.png" />
+  <h1>HALLOWNEST BINGO</h1>
+  <img src="%sveltekit.assets%/HK-hearder.png" />
+</header>
 
 <nav>
   <a href="/">GAME</a>

a978dcf5cd1a070f7bafa56f03685f6b42911219
Thomas Petit <6poules@gmail.com>
2023-03-21 17:34:35 +0100

    [+] footer update

--- hallownest_bingo/src/routes/+page.svelte
+++ hallownest_bingo/src/routes/+page.svelte
@@ -93,34 +93,39 @@
     <section class="option-display">
       <h4>PLACEHOLDER</h4>
       <div class="button-list">
-        <button>placeholder</button>
-        <button>placeholder</button>
-        <button>placeholder</button>
+        <button class="inter-button">X</button>
+        <button class="inter-button">X</button>
+        <button class="inter-button">X</button>
       </div>
     </section>
     <section class="option-display">
       <h4>PLACEHOLDER</h4>
       <div class="button-list">
-        <button>placeholder</button>
-        <button>placeholder</button>
-        <button>placeholder</button>
-        <button>placeholder</button>
-        <button>placeholder</button>
-        <button>placeholder</button>
-        <button>placeholder</button>
+        <button class="inter-button">X</button>
+        <button class="inter-button">X</button>
+        <button class="inter-button">X</button>
+        <button class="inter-button">X</button>
+        <button class="inter-button">X</button>
+        <button class="inter-button">X</button>
+        <button class="inter-button">X</button>
       </div>
     </section>
   </aside>
 </main>
 
 <footer>
-  <h4>PLACEHOLDER</h4>
+  <h2>PLACEHOLDER</h2>
   <section class="bingo-generator">
-    <span>placeholder</span>
-    <form>
-      <span>SEED</span>
+    <div class="button-list">
+      <button class="inter-button">X</button>
+      <button class="inter-button">X</button>
+      <button class="inter-button">X</button>
+      <button class="inter-button">X</button>
+    </div>
+    <form class="seed-form">
+      <span class="padding-5px">SEED :</span>
       <input type="text" />
-      <input type="button" value="generate" />
+      <input type="button" value="RUN" class="inter-button" />
     </form>
   </section>
 </footer>

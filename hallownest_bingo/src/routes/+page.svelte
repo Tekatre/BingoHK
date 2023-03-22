1a8b31149dda84aa88de0f3ecdf2b99aa3cfe913
Thomas Petit <6poules@gmail.com>
2023-03-22 21:50:59 +0100

    [+] selector corners

--- hallownest_bingo/src/routes/+page.svelte
+++ hallownest_bingo/src/routes/+page.svelte
@@ -95,21 +95,31 @@
     <section class="option-display">
       <h4>SELECTION</h4>
       <div class="button-list">
-        <button class="inter-button">X</button>
-        <button class="inter-button">X</button>
-        <button class="inter-button">X</button>
+        <button class="inter-button"><div class="button-handler">X</div></button
+        >
+        <button class="inter-button"><div class="button-handler">X</div></button
+        >
+        <button class="inter-button"><div class="button-handler">X</div></button
+        >
       </div>
     </section>
     <section class="option-display">
       <h4>THEME</h4>
       <div class="button-list">
-        <button class="inter-button">X</button>
-        <button class="inter-button">X</button>
-        <button class="inter-button">X</button>
-        <button class="inter-button">X</button>
-        <button class="inter-button">X</button>
-        <button class="inter-button">X</button>
-        <button class="inter-button">X</button>
+        <button class="inter-button"><div class="button-handler">X</div></button
+        >
+        <button class="inter-button"><div class="button-handler">X</div></button
+        >
+        <button class="inter-button"><div class="button-handler">X</div></button
+        >
+        <button class="inter-button"><div class="button-handler">X</div></button
+        >
+        <button class="inter-button"><div class="button-handler">X</div></button
+        >
+        <button class="inter-button"><div class="button-handler">X</div></button
+        >
+        <button class="inter-button"><div class="button-handler">X</div></button
+        >
       </div>
     </section>
   </aside>
@@ -119,15 +129,17 @@
   <h2>GENERATE GRID</h2>
   <section class="bingo-generator">
     <div class="button-list">
-      <button class="inter-button">X</button>
-      <button class="inter-button">X</button>
-      <button class="inter-button">X</button>
-      <button class="inter-button">X</button>
+      <button class="inter-button selected">
+        <div class="button-handler">X</div></button
+      >
+      <button class="inter-button"><div class="button-handler">X</div></button>
+      <button class="inter-button"><div class="button-handler">X</div></button>
+      <button class="inter-button"><div class="button-handler">X</div></button>
     </div>
     <form class="seed-form">
       <span>SEED :</span>
       <input type="text" />
-      <input type="button" value="RUN" class="inter-button" />
+      <input type="button" value="RUN" class="run-button" />
     </form>
   </section>
 </footer>

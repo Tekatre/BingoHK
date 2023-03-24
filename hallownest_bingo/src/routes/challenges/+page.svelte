e3d687631026f88ab4952f6bc6750b235837c408
Thomas Petit <6poules@gmail.com>
2023-03-24 19:00:34 +0100

    [+] challenges display

--- hallownest_bingo/src/routes/challenges/+page.svelte
+++ hallownest_bingo/src/routes/challenges/+page.svelte
@@ -1,48 +1,58 @@
 <script lang="ts">
-  import chall from "../../challenges.json";
-  const display: string[] = [];
-  const boss_list: string[] = [];
-  const item_list: string[] = [];
-  const charm_list: string[] = [];
-  const coll_list: string[] = [];
-  const dream_boss_list: string[] = [];
-  const event_list: string[] = [];
-  chall.forEach((item) => {
-    switch (item.kind) {
-      case "boss":
-        boss_list.push(item.desc_fr);
-        break;
-      case "item":
-        item_list.push(item.desc_fr);
-        break;
-      case "charm":
-        charm_list.push(item.desc_fr);
-        break;
-      case "coll":
-        coll_list.push(item.desc_fr);
-        break;
-      case "db":
-        dream_boss_list.push(item.desc_fr);
-        break;
-      default:
-        event_list.push(item.desc_fr);
-        break;
-    }
-  });
+  import challs from "../../public/challenges.json";
+  console.log("oui ça démarre");
 </script>
 
 <h1>Challenges page</h1>
-
-<p>{display}</p>
-<br />
-<p>{boss_list}</p>
-<br />
-<p>{item_list}</p>
-<br />
-<p>{charm_list}</p>
-<br />
-<p>{coll_list}</p>
-<br />
-<p>{dream_boss_list}</p>
-<br />
-<p>{dream_boss_list}</p>
+<main>
+  <div class="chall-displays">
+    <div class="chall-table" id="chall-boss">
+      <th>BOSSES</th>
+      {#each challs as chall}
+        {#if chall.kind == "boss"}
+          <tr>{chall.desc_fr}</tr>
+        {/if}
+      {/each}
+    </div>
+    <div class="chall-table" id="chall-item">
+      <th>ITEMS</th>
+      {#each challs as chall}
+        {#if chall.kind == "item"}
+          <tr>{chall.desc_fr}</tr>
+        {/if}
+      {/each}
+    </div>
+    <div class="chall-table" id="chall-charm">
+      <th>CHARMS</th>
+      {#each challs as chall}
+        {#if chall.kind == "charm"}
+          <tr>{chall.desc_fr}</tr>
+        {/if}
+      {/each}
+    </div>
+    <div class="chall-table" id="chall-db">
+      <th>DREAM BOSSES</th>
+      {#each challs as chall}
+        {#if chall.kind == "db"}
+          <tr>{chall.desc_fr}</tr>
+        {/if}
+      {/each}
+    </div>
+    <div class="chall-table" id="chall-event">
+      <th>EVENTS</th>
+      {#each challs as chall}
+        {#if chall.kind == "event"}
+          <tr>{chall.desc_fr}</tr>
+        {/if}
+      {/each}
+    </div>
+    <div class="chall-table" id="chall-coll">
+      <th>COLLECTIBLE</th>
+      {#each challs as chall}
+        {#if chall.kind == "coll"}
+          <tr>{chall.desc_fr}</tr>
+        {/if}
+      {/each}
+    </div>
+  </div>
+</main>

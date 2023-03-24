0a4610df4d6cbf301db8074ea500d4bc2f7e64fd
Thomas Petit <6poules@gmail.com>
2023-03-24 14:06:18 +0100

    [-] revert on the challenge add form

--- hallownest_bingo/src/routes/challenges/+page.svelte
+++ hallownest_bingo/src/routes/challenges/+page.svelte
@@ -33,22 +33,6 @@
 
 <h1>Challenges page</h1>
 
-<form>
-  <input type="text" id="name" placeholder="name" />
-  <select>
-    <option value="boss">boss</option>
-    <option value="item">item</option>
-    <option value="charm">charm</option>
-    <option value="coll">collectible</option>
-    <option value="db">dream boss</option>
-    <option value="event">boss</option>
-  </select>
-  <input type="text" id="desc_en" placeholder="English desc" />
-  <input type="Text" id="desc_fr" placeholder="French desc" />
-  <input type="number" id="diff" value="1" />
-  <input type="button" value="add" />
-</form>
-
 <p>{display}</p>
 <br />
 <p>{boss_list}</p>

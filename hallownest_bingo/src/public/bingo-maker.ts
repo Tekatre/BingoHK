47d0be55a6efd1cc3defe4a9f27ac3a4ec4c4435
Thomas Petit <6poules@gmail.com>
2023-04-07 17:10:19 +0200

    [+] seed gcl step

--- hallownest_bingo/src/public/bingo-maker.ts
+++ hallownest_bingo/src/public/bingo-maker.ts
@@ -2,17 +2,32 @@
 let grid_id : number[][] = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]];
 
 /**
- * Upate the golbal variable grid_id with the ids of challenges 
- * @param seed : a randomly generated integer
- * @param entries : the list of ids of all the entries that can appear in the bingo grid 
+ * Upate the grid with the ids of challenges in the list of entries
+ * @param grid  a matrix representing the bingo grid
+ * @param seed  a randomly generated integer
+ * @param entries  the list of ids of all the entries that can appear in the bingo grid 
  */
-function build_grid_id(seed:number, entries:number[]) {
-  for (let row = 0; row < grid_id.length; row++) {
-    for (let line = 0; line < grid_id[row].length; line++) {
-      let index = seed % entries.length;
-      grid_id[row][line] = entries[index]
+function build_grid_id(grid:number[][], seed:number, entries:number[]) : number[][]{
+  for (let row = 0; row < grid.length; row++) {
+    for (let line = 0; line < grid[row].length; line++) {
+      seed = gcl_step(seed)
+      let index = seed % entries.length
+      grid[row][line] = entries[index]
       entries.splice(index,1)
     }
   }
+  return grid
 }
 
+let coef_a : number = 74;
+let coef_c : number = 75;
+let modulo : number = 2 ** 16+1;
+
+/**
+ * Take one step on the seed wthout any risk of cycling to a previous step
+ * @param seed  a randomly generated integer
+ * @returns  The seed after one step
+ */
+function gcl_step(seed:number) : number {
+  return (coef_a * seed + coef_c) % modulo;
+}

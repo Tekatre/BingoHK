e68f9942756e7ab524143c7e4e82d4cd5db2f994
Thomas Petit <6poules@gmail.com>
2023-04-09 23:52:38 +0200

    [+] line, column & diagonal completion observer

--- hallownest_bingo/src/public/bingo-manager.ts
+++ hallownest_bingo/src/public/bingo-manager.ts
@@ -9,3 +9,73 @@ export function cell_select_switch(bingo_cell:HTMLButtonElement, win_grid : numb
     win_grid[Math.floor(button_id/10)][button_id-(Math.floor(button_id/10))*10] = 1
   }
 }
+
+export function check_win(win_grid: number [][]) {
+  clear_grid_win(win_grid);
+  check_line(win_grid);
+  check_column(win_grid);
+  check_diag(win_grid);
+}
+
+function clear_grid_win(win_grid:number[][]) {
+  for (let line = 0; line < win_grid.length; line++) {
+    for (let column = 0; column < win_grid.length; column++) {
+      document.getElementById(line.toString()+column.toString())?.getElementsByClassName("cell-handler")[0].classList.remove("win")
+    }
+  }
+}
+
+function check_line(grid: number[][]) {
+  for (let line = 0; line < grid.length; line++) {
+    let buffer : number = 1
+    for (let column = 0; column < grid[line].length; column++) {
+      buffer = buffer * grid[line][column];
+    }
+    if (buffer == 1) {
+      switch_win_line(line, grid[line].length)
+    }
+  }
+}
+
+function switch_win_line(line:number, n_column:number) {
+  for (let index = 0; index < n_column; index++) {
+    document.getElementById(line.toString()+index.toString())?.getElementsByClassName("cell-handler")[0].classList.add("win")
+  }
+}
+
+function check_column(grid : number[][]) {
+  for (let column = 0; column < grid[0].length; column++) {
+    let buffer :number = 1;
+    for (let line = 0; line < grid.length; line++) {
+      buffer = buffer * grid[line][column];
+    }
+    if (buffer == 1) {
+      switch_win_column(column, grid.length)
+    }
+  }
+}
+
+function switch_win_column(column:number, n_line:number) {
+  for (let index = 0; index < n_line; index++) {
+    document.getElementById(index.toString()+column.toString())?.getElementsByClassName("cell-handler")[0].classList.add("win")
+  }
+}
+
+function check_diag(grid :number[][]) {
+  let NW_buffer = 1
+  let NE_buffer = 1
+  for (let index = 0; index < grid.length; index++) {
+    NW_buffer = NW_buffer * grid[index][index]
+    NE_buffer = NE_buffer * grid[grid.length - 1 - index][index]
+  }
+  if (NW_buffer == 1) {
+    for (let index = 0; index < grid.length; index++) {
+      document.getElementById(index.toString()+index.toString())?.getElementsByClassName("cell-handler")[0].classList.add("win")
+    }
+  }
+  if (NE_buffer == 1) {
+    for (let index = 0; index < grid.length; index++) {
+      document.getElementById((grid.length - 1 - index).toString()+index.toString())?.getElementsByClassName("cell-handler")[0].classList.add("win")
+    }
+  }
+}
--- hallownest_bingo/src/routes/+page.svelte
+++ hallownest_bingo/src/routes/+page.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
   import type { PageData } from "./$types";
   import { translate } from "../public/bingo-maker";
-  import { cell_select_switch } from "../public/bingo-manager";
+  import { cell_select_switch, check_win } from "../public/bingo-manager";
 
   export let data: PageData;
 
   function handle_cell_click(event: MouseEvent) {
     if (event.currentTarget instanceof HTMLButtonElement) {
       cell_select_switch(event.currentTarget, win_grid);
+      check_win(win_grid);
     } else {
       console.log(typeof event.currentTarget);
     }
-    console.log(win_grid);
+    //console.log(win_grid);
   }
 
   let win_grid: number[][] = [
@@ -30,7 +31,7 @@
   <section class="bingo-grid">
     <div class="bingo-row">
       <button class="bingo-cell" id="00" on:click={handle_cell_click}
-        ><div class="cell-handler win">
+        ><div class="cell-handler">
           <span class="cell-text"
             >{translate(grid_id[0][0], lang, data.translator.trans)}</span
           >
@@ -43,14 +44,14 @@
           >
         </div></button
       >
-      <button class="bingo-cell selected" id="02" on:click={handle_cell_click}
+      <button class="bingo-cell" id="02" on:click={handle_cell_click}
         ><div class="cell-handler">
           <span class="cell-text"
             >{translate(grid_id[0][2], lang, data.translator.trans)}</span
           >
         </div></button
       >
-      <button class="bingo-cell selected" id="03" on:click={handle_cell_click}
+      <button class="bingo-cell" id="03" on:click={handle_cell_click}
         ><div class="cell-handler">
           <span class="cell-text"
             >{translate(grid_id[0][3], lang, data.translator.trans)}</span
@@ -80,7 +81,7 @@
           >
         </div></button
       >
-      <button class="bingo-cell selected" id="12" on:click={handle_cell_click}
+      <button class="bingo-cell" id="12" on:click={handle_cell_click}
         ><div class="cell-handler">
           {translate(grid_id[1][2], lang, data.translator.trans)}
         </div></button
@@ -122,7 +123,7 @@
           >
         </div></button
       >
-      <button class="bingo-cell selected" id="23" on:click={handle_cell_click}
+      <button class="bingo-cell" id="23" on:click={handle_cell_click}
         ><div class="cell-handler">
           <span class="cell-text"
             >{translate(grid_id[2][3], lang, data.translator.trans)}</span
@@ -228,19 +229,11 @@
     <section class="option-display">
       <h4>THEME</h4>
       <div class="button-list">
-        <button class="inter-button"><div class="button-handler">X</div></button
+        <button class="inter-button"><div class="button-handler">W</div></button
         >
-        <button class="inter-button"><div class="button-handler">X</div></button
+        <button class="inter-button"><div class="button-handler">I</div></button
         >
-        <button class="inter-button"><div class="button-handler">X</div></button
-        >
-        <button class="inter-button"><div class="button-handler">X</div></button
-        >
-        <button class="inter-button"><div class="button-handler">X</div></button
-        >
-        <button class="inter-button"><div class="button-handler">X</div></button
-        >
-        <button class="inter-button"><div class="button-handler">X</div></button
+        <button class="inter-button"><div class="button-handler">P</div></button
         >
       </div>
     </section>
